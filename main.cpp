b4eaacd6beb43a721b08b8a7f32fd68b1d7f886a
pieromarini <piero.marini21@gmail.com>
2019-08-22 18:57:53 -0500

    Solution

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 
-using namespace std;
-
 int main() {
-	return EXIT_SUCCESS;
-}
+  int T, N;
 
+  std::cin >> T;
+
+  for (int k = 1; k < T + 1; ++k) {
+	std::cin >> N;
+	int h = 0, l = 0, curr = 0, prev = 0;
+
+	std::cin >> prev;
+	for (int i = 1; i < N; ++i) {
+	  std::cin >> curr;
+	  if (curr > prev) h++;
+	  if (curr < prev) l++;
+	  prev = curr;
+	}
+
+	std::cout << "Case " << k << ": " << h << ' ' << l << '\n';
+  }
+}
